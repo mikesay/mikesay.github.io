e5644c4253c7ab9c5e368bfae9c4848301aaac42
mikejianzhang <mikejianzhang@163.com>
2026-06-22 05:15:22 +0000

    Deploy site

---
+++ static/api/js/share/combine_api.js
@@ -0,0 +1,1 @@
+window._bd_share_main.F.module("share/combine_api",function(n,a,i){n("base/tangram").T;var e=n("base/class").Class,s=n("share/api_base");a.CombineApi=e.create(function(n){var a=null,i=null;this.setApi=function(n,e){a=n,i=e},this._init=function(){a&&i&&a.on("sharecompleted",function(n){})}},s.ApiBase)});
